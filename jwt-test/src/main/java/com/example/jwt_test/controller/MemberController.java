3b53b2b1f0b278100a46736b73a8108efac5d203
kickbell <dev.kickbell@gmail.com>
2026-02-10 10:52:28 +0900

    feat: getList 추가

--- jwt-test/src/main/java/com/example/jwt_test/controller/MemberController.java
+++ jwt-test/src/main/java/com/example/jwt_test/controller/MemberController.java
@@ -1,15 +1,29 @@
 package com.example.jwt_test.controller;
 
+import com.example.jwt_test.dto.MemberDto;
+import com.example.jwt_test.dto.MemberRegDto;
+import com.example.jwt_test.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
     @GetMapping
-    public ResponseEntity<String> getList(){
-        return ResponseEntity.ok("모든 데이터 전송");
+    public ResponseEntity<List<MemberDto>> getList(){
+        return ResponseEntity.ok(memberService.getList() );
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> register(
+            @ParameterObject @ModelAttribute MemberRegDto memberRegDto){
+        memberService.register( memberRegDto );
+        return ResponseEntity.ok().build();
     }
 }
--- jwt-test/src/main/java/com/example/jwt_test/service/MemberService.java
+++ jwt-test/src/main/java/com/example/jwt_test/service/MemberService.java
@@ -1,4 +1,28 @@
 package com.example.jwt_test.service;
 
+import com.example.jwt_test.dto.MemberDto;
+import com.example.jwt_test.exception.MemberNotFoundException;
+import com.example.jwt_test.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public List<MemberDto> getList(){
+        List<MemberDto> list = memberRepository.findAll()
+                .stream().map(MemberDto::new).toList();
+
+        if(list.isEmpty())
+            throw new MemberNotFoundException("저장된 데이터 없음");
+
+        return list;
+    }
 }
