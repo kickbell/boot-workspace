6055aaa8261caf2a41dc9450dd2e4b51ae02078a
kickbell <dev.kickbell@gmail.com>
2026-02-25 01:03:49 +0900

    feat: WebSecurityConfig 추가하고 필요없는 WebConfig는 주석처리

---
+++ basic/src/main/java/com/ex01/basic/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.ex01.basic.config;
+
+import com.ex01.basic.exception.JwtAccessDeniedHandler;
+import com.ex01.basic.exception.JwtAuthEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+    @Autowired
+    private JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    @Autowired
+    private JwtAuthEntryPoint jwtAuthEntryPoint;
+    @Autowired
+    JwtFilter jwtFilter;
+    @Bean
+    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/members").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/members").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**"
+                        ).permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated() //그 외 모든 경로는 인증해야 한다.
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling( e -> e
+                        .authenticationEntryPoint(jwtAuthEntryPoint) //인증되지 않은 사용자 접근
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
